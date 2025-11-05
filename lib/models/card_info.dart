4bf7250e33ea8bcc769b4fee94060ef55bb72072
Collin Dietz <collin.dietz@geappliances.com>
2025-11-04 20:13:10 -0500

    All cards pulled progromatically

--- lib/models/card_info.dart
+++ lib/models/card_info.dart
@@ -1,6 +1,29 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 class CardInfo {
   final String name;
   final String url;
 
   CardInfo({required this.name, required this.url});
+
+  factory CardInfo.fromJson(Map<String, dynamic> json) {
+    return CardInfo(
+      name: json['container']['json_dict']['card']['name'] as String,
+      url:
+          json['container']['json_dict']['card']['image_uris'][0]['normal']
+              as String,
+    );
+  }
+
+  static Future<CardInfo> fromUrl(String url) async {
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    if (response.statusCode != 200) {
+      throw Exception('Failed to load commander data');
+    }
+    final jsonData = json.decode(response.body);
+    return CardInfo.fromJson(jsonData);
+  }
 }
--- lib/models/commander.dart
+++ lib/models/commander.dart
@@ -1,17 +1,33 @@
 import 'dart:convert';
+import 'package:commander_tinder/models/card_info.dart';
 import 'package:http/http.dart' as http;
 
 class Commander {
   final String name;
   final String image_url;
+  final List<String> cardJsonUrls;
 
-  Commander({required this.name, required this.image_url});
+  Commander({
+    required this.name,
+    required this.image_url,
+    required this.cardJsonUrls,
+  });
 
   factory Commander.fromJson(Map<String, dynamic> json) {
+    final List<dynamic> cardLists = json['container']['json_dict']['cardlists'];
+
+    List<String> urls = [];
+    for (Map<String, dynamic> cardList in cardLists) {
+      for (Map<String, dynamic> card in cardList['cardviews']) {
+        urls.add('https://json.edhrec.com/pages${card['url']}.json');
+      }
+    }
+
     return Commander(
       name: json['container']['json_dict']['card']['name'] ?? '',
       image_url:
           json['container']['json_dict']['card']['image_uris'][0]['normal'],
+      cardJsonUrls: urls,
     );
   }
 
@@ -23,5 +39,9 @@ class Commander {
     }
     final jsonData = json.decode(response.body);
     return Commander.fromJson(jsonData);
+  }
+
+  Future<CardInfo> getCard(int index) {
+    return CardInfo.fromUrl(cardJsonUrls[index]);
   }
 }
