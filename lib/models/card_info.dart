21d47cb46cd9d3a70e5249e266754b726ca0f0a2
Collin Dietz <collin.dietz@geappliances.com>
2025-11-11 01:46:05 -0500

    Not all commanders are GC

--- lib/models/card_info.dart
+++ lib/models/card_info.dart
@@ -48,7 +48,7 @@ class CardInfo {
 
     final bool isGameChange = scryfallJson != null
         ? scryfallJson['game_changer']
-        : true;
+        : false;
 
     return CardInfo(
       name: cardJson['name'] as String,
